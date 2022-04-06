ba2a6e89b2657319b8965e41c68fef4373b57e87
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2022-04-06 10:40:19 -0400

    add skeleton of models

--- emergencyroom/staff/models.py
+++ emergencyroom/staff/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.model):
+    pass
+
+
+class Symptoms(models.model):
+    pass
+
+
+class Allergies(models.model):
+    pass
+
+
+class Test(models.model):
+    pass
+
+
+class Medication(models.model):
+    pass
+
+
+class ReligiousRestrictions(models.model):
+    pass
+
+
+class EmergencyContact(models.model):
+    pass
+
+
+class Diagnose(models.model):
+    pass
+
+
+class CovidVaccineInfo(models.Model):
+    pass
+
+
+class CovidVaccineShot(models.Model):
+    pass
