a01e832b7b2352cccf98b66fef46c511e4af275b
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2022-04-06 10:53:06 -0400

    add patient fields

--- emergencyroom/staff/models.py
+++ emergencyroom/staff/models.py
@@ -1,15 +1,25 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+import datetime
+import uuid  # Required for unique book instances
+from decimal import Decimal
 
 
 class Patient(models.model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    date_of_birth = models.DateField(null=True, Blank=True)
+    height = models.DoubleField(help_text='Height in cm', validators=[MinValueValidator(Decimal('0.01'))])
+    weight = models.DoubleField(help_text='Weight in kg,', validators=[MinValueValidator(Decimal('0.01'))])
+    heart_rate = models.PositiveIntegerField(null=True)
+    symptoms = models.ManyToManyField('Symptom')
+    allergies = models.ManyToManyField('Allergy')
+
+
+class Symptom(models.model):
     pass
 
 
-class Symptoms(models.model):
-    pass
-
-
-class Allergies(models.model):
+class Allergy(models.model):
     pass
 
 
