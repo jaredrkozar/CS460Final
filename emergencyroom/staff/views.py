8fb106f2b17f0bc46f29576a1596aaf0fde503f7
zachary <zbiernat2000@gmail.com>
2022-04-11 15:23:11 -0400

    fix patient view

--- emergencyroom/staff/views.py
+++ emergencyroom/staff/views.py
@@ -5,20 +5,27 @@ from django.contrib.auth.decorators import login_required, permission_required
 from .models import Patient
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+
 def index(request):
     context = {}
     return render(request, 'index.html', context=context)
 
-#@login_required
-#@permission_required("medical professional", raise_exception=True)
+
+# @login_required
+# @permission_required("medical professional", raise_exception=True)
 class PatientListView(generic.ListView):
     model = Patient
     paginate_by = 10
 
-class PatientDetailView(generic.DetailView):
-    model = Patient
+
+def patient_view(request, pk):
+    patient = Patient.objects.get(pk=pk)
+    context = {'patient' : patient}
+    return render(request, 'staff/patient_detail.html', context)
+
 
 class CreatePatient(CreateView):
     model = Patient
-    fields = ['first_name', 'last_name', 'date_of_birth', 'height', 'weight', 'heart_rate', 'blood_pressure_upper', 'blood_pressure_lower', 'religious_restriction', 'doctor_note', 'nurse_note', 'nights_stayed', 'drug_usage', 'discharge_instructions', 'gender', 'race', 'sexual_active', 'IV', 'blood_type']
-
+    fields = ['first_name', 'last_name', 'date_of_birth', 'height', 'weight', 'heart_rate', 'blood_pressure_upper',
+              'blood_pressure_lower', 'religious_restriction', 'doctor_note', 'nurse_note', 'nights_stayed',
+              'drug_usage', 'discharge_instructions', 'gender', 'race', 'sexual_active', 'IV', 'blood_type']
