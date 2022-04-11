ae5a3d8e94377ed75486dce8a6b3fb71f1de30e8
Jared Kozar <jared.rkozar@gmail.com>
2022-04-11 15:09:18 -0400

    added fields to form

--- emergencyroom/staff/urls.py
+++ emergencyroom/staff/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('patients/', views.PatientListView.as_view(), name='patients'),
-    path('patient/<int:pk>', views.PatientDetailView.as_view(), name='patient-detail'),
+    path('newPatient', views.CreatePatient.as_view(), name='new_patient'),
 ]
--- emergencyroom/staff/views.py
+++ emergencyroom/staff/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Patient
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def index(request):
     context = {}
@@ -18,25 +18,7 @@ class PatientListView(generic.ListView):
 class PatientDetailView(generic.DetailView):
     model = Patient
 
-
-class EditPatient(forms.Form):
-    first_name_field = forms.CharField(max_length=20)
-    last_name_field = forms.CharField(max_length=20)
-    birth_date_field =  forms.DateField(help_text="Enter the patients birth date")
-    height_field = forms.DecimalField(min_value=1.0)
-    weight_field = forms.IntegerField()
-    heart_rate_field = forms.IntegerField()
-    blood_pressure_upper_field = forms.IntegerField()
-    blood_pressure_lower_field = forms.IntegerField()
-    first_name_field = forms.CharField(help_text="enter any religious restrictions this patient has.")
-    doctor_note_field = forms.CharField("Doctors notes")
-    nurse_note_field = forms.CharField("Nurses notes")
-    nights_stayed_field = forms.IntegerField()
-    drug_usage_field = forms.BooleanField()
-    discharge_instructions_field =  forms.CharField(help_text="Eneter the discharge instructions for this patient")
-    gender_field = forms.CharField(help_text="Enter the patients gender")
-    race_field = forms.CharField(help_text="Enter the patients gender")
-    sexually_active_field = forms.BooleanField()
-    IV_field = forms.BooleanField()
-    bood_type_field = forms.CharField(help_text="Enter the patients blood type")
+class CreatePatient(CreateView):
+    model = Patient
+    fields = ['first_name', 'last_name', 'date_of_birth', 'height', 'weight', 'heart_rate', 'blood_pressure_upper', 'blood_pressure_lower', 'religious_restriction', 'doctor_note', 'nurse_note', 'nights_stayed', 'drug_usage', 'discharge_instructions', 'gender', 'race', 'sexual_active', 'IV', 'blood_type']
 
