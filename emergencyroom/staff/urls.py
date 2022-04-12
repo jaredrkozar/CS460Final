7655229069ed4091d43f44dac4012f0f5d5148fb
Jared Kozar <jared.rkozar@gmail.com>
2022-04-11 23:17:05 -0400

    add emergency contact detail view

--- emergencyroom/staff/urls.py
+++ emergencyroom/staff/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('patient/<int:pk>', views.patient_view, name='patient-detail'),
     path('newPatient', views.CreatePatientView.as_view(), name='new_patient'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
